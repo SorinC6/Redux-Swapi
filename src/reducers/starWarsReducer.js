4d37772e6f5fc215544385034e84204fa53d8852
Sorin Chis <kish.sorin@yahoo.com>
2019-03-06 14:21:26 +0200

    reducer added

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,17 +1,32 @@
-import {FETCH_CHAR_START,FETCH_CHAR_SUCCESS,FETCH_CHAT_FAIL} from  '../actions/index';
+import { FETCH_CHAR_START, FETCH_CHAR_SUCCESS, FETCH_CHAT_FAIL } from '../actions/index';
 const initialState = {
-  characters: [],
-  isLoading:false,
-  error:null
-  // Array characters, Boolean fetching, null error.
+	characters: [],
+	isLoading: false,
+	error: null
+	// Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
-  switch (action.type) {
-      case 
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case FETCH_CHAR_START:
+			return {
+				...state,
+				error: '',
+				isLoading: true
+			};
+		case FETCH_CHAR_SUCCESS:
+			return {
+				...state,
+				characters: action.payload,
+				error: '',
+				isLoading: false
+			};
+		case FETCH_CHAT_FAIL:
+			return {
+				...state,
+				error: action.payload,
+				isLoading: false
+			};
+		default:
+			return state;
+	}
 };
